97fc940786feb4f74be6dd7be84cfab69036029b
佐々木美咲 <the.glass007@icloud.com>
2021-04-21 18:10:18 +0900

    プレハブ化したParticleをいつでもどこでも気軽に呼べるようにするための下準備&余計なのを消しました。

---
+++ purot/Assets/Effect/Script/CEffect.cs
@@ -0,0 +1,27 @@
+﻿/*==============================================================================
+    PROJECT NAME
+    [CEffect.cs]
+    ・エフェクトを再生する（シングルトンにしたからどこでもいつでも呼べます）
+--------------------------------------------------------------------------------
+    2021.04.21 @Author Misaki Sasaki
+================================================================================
+    History
+        YYMMDD NAME
+            UPDATE LOG
+            
+/*============================================================================*/
+using UnityEngine;
+using System.Collections;
+
+public class CEffect : CSingletonMonoBehaviour<CEffect>
+{
+    // パーティクルを格納する変数
+    // インスペクタ上で再生したいParticleのPrefabを入れて番号指定で再生
+    // ただし、エフェクト再生位置は別途セルフで設定してください。
+    [SerializeField] private ParticleSystem[] psEffect; 
+
+    // パーティクルエフェクトを再生させる（引数・番号指定）
+    public void PlayEffect(int num){
+        psEffect[num].Play(true);
+    }
+}
---
+++ purot/Assets/Effect/Script/CSingletonMonoBehaviour.cs
@@ -0,0 +1,55 @@
+﻿/*==============================================================================
+    PROJECT NAME
+    [CSingletonMonoBehaviour.cs]
+    ・シングルトン（これ以上言えることがありませんでした。）
+--------------------------------------------------------------------------------
+    2021.04.21 @Author Misaki Sasaki
+================================================================================
+    History
+        YYMMDD NAME
+            UPDATE LOG
+            
+/*============================================================================*/
+
+using UnityEngine;
+using System;
+
+public abstract class CSingletonMonoBehaviour<T> : MonoBehaviour where T : MonoBehaviour
+{
+
+    private static T instance;
+    public static T Instance{
+        get{
+            // インスタンスがない場合はデバッグログで表示する
+            if (instance == null){
+                Type t = typeof(T);
+                instance = (T)FindObjectOfType(t);
+
+                if (instance == null){
+                    Debug.LogError(t + " をアタッチしているGameObjectはありません");
+                }
+            }
+
+            return instance;
+        }
+    }
+
+    virtual protected void Awake(){
+        // 他のゲームオブジェクトにアタッチされているか調べる
+        // アタッチされている場合は破棄する
+        CheckInstance();
+    }
+
+    protected bool CheckInstance(){
+        if (instance == null){
+            instance = this as T;
+            return true;
+        }
+        else if (Instance == this){
+            return true;
+        }
+
+        Destroy(this);
+        return false;
+    }
+}
