111ca5556701a6e6e3c7267d4f907f51d2269597
佐々木美咲 <the.glass007@icloud.com>
2021-05-15 16:28:27 +0900

    一旦あげたいやつあげた（わしの）

--- purot/Assets/UI/Sprite/Setting/CSetting.cs
+++ purot/Assets/UI/Sprite/Setting/CSetting.cs
@@ -3,60 +3,55 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Audio;
 
-
 public class CSetting : MonoBehaviour
 {
-    [SerializeField] AudioMixer audioMixer = default;
-    [SerializeField] GameObject setting_buttonObj;
+    /* 音量設定関連 */
+    [SerializeField] AudioMixer mAudioMixer = default;
 
-    [SerializeField] GameObject vol1 = default;
-    [SerializeField] GameObject vol2 = default;
-    [SerializeField] GameObject vol3 = default;
-    [SerializeField] GameObject vol4 = default;
+    [SerializeField] GameObject gVol_1 = default;   // 音量を表している画像を格納（1）
+    [SerializeField] GameObject gVol_2 = default;   // 音量を表している画像を格納（2）
+    [SerializeField] GameObject gVol_3 = default;   // 音量を表している画像を格納（3）
+    [SerializeField] GameObject gVol_4 = default;   // 音量を表している画像を格納（4）
 
-    [SerializeField] public static int NowVol = 2;
-    private bool bPanelOn;
+    [SerializeField] public static int iNowVol = 2; // 初期Volume
 
-    // Start is called before the first frame update
+    /* 設定パネル表示・非表示関連 */
+    [SerializeField] GameObject gSettingObj;        // 設定パネル自体を格納
+    private bool bPanelOn;                          // パネルの表示・非表示のフラグ
+
     void Start()
     {
         bPanelOn = false;
+        gSettingObj = GameObject.Find("PFB_SettingButton");
+        gSettingObj.SetActive(false);
 
-        vol1 = GameObject.Find("vol1");
-        vol2 = GameObject.Find("vol2");
-        vol3 = GameObject.Find("vol3");
-        vol4 = GameObject.Find("vol4");
-
-        setting_buttonObj = GameObject.Find("PFB_SettingButton");
-        setting_buttonObj.SetActive(false);
-
-        if (NowVol == 1)
+        if (iNowVol == 1)
         {
-            vol1.SetActive(true);
-            vol2.SetActive(false);
-            vol3.SetActive(false);
-            vol4.SetActive(false);
+            gVol_1.SetActive(true);
+            gVol_2.SetActive(false);
+            gVol_3.SetActive(false);
+            gVol_4.SetActive(false);
         }
-        else if (NowVol == 2)
+        else if (iNowVol == 2)
         {
-            vol1.SetActive(true);
-            vol2.SetActive(true);
-            vol3.SetActive(false);
-            vol4.SetActive(false);
+            gVol_1.SetActive(true);
+            gVol_2.SetActive(true);
+            gVol_3.SetActive(false);
+            gVol_4.SetActive(false);
         }
-        else if (NowVol == 3)
+        else if (iNowVol == 3)
         {
-            vol1.SetActive(true);
-            vol2.SetActive(true);
-            vol3.SetActive(true);
-            vol4.SetActive(false);
+            gVol_1.SetActive(true);
+            gVol_2.SetActive(true);
+            gVol_3.SetActive(true);
+            gVol_4.SetActive(false);
         }
-        else if (NowVol == 4)
+        else if (iNowVol == 4)
         {
-            vol1.SetActive(true);
-            vol2.SetActive(true);
-            vol3.SetActive(true);
-            vol4.SetActive(true);
+            gVol_1.SetActive(true);
+            gVol_2.SetActive(true);
+            gVol_3.SetActive(true);
+            gVol_4.SetActive(true);
         }
     }
 
@@ -75,92 +70,97 @@ public class CSetting : MonoBehaviour
     // 音量UP
     public void OnVolUp()
     {
-        if (NowVol == 1)
+        if (iNowVol == 1)
         {
             Debug.Log("音量２に");
-            audioMixer.SetFloat("SE", 0.0f);
-            audioMixer.SetFloat("BGM", 0.0f);
+            mAudioMixer.SetFloat("SE", 0.0f);
+            mAudioMixer.SetFloat("BGM", 0.0f);
 
-            vol1.SetActive(true);
-            vol2.SetActive(true);
-            vol3.SetActive(false);
+            gVol_1.SetActive(true);
+            gVol_2.SetActive(true);
+            gVol_3.SetActive(false);
+            gVol_4.SetActive(false);
 
-            NowVol++;
+            iNowVol++;
         }
-        else if (NowVol == 2)
+        else if (iNowVol == 2)
         {
             Debug.Log("音量３に");
-            audioMixer.SetFloat("SE", 15.0f);
-            audioMixer.SetFloat("BGM", 15.0f);
+            mAudioMixer.SetFloat("SE", 15.0f);
+            mAudioMixer.SetFloat("BGM", 15.0f);
 
-            vol1.SetActive(true);
-            vol2.SetActive(true);
-            vol3.SetActive(true);
+            gVol_1.SetActive(true);
+            gVol_2.SetActive(true);
+            gVol_3.SetActive(true);
+            gVol_4.SetActive(false);
 
-            NowVol++;
+            iNowVol++;
         }
-        else if (NowVol == 3)
+        else if (iNowVol == 3)
         {
             Debug.Log("音量４に");
-            audioMixer.SetFloat("SE", 30.0f);
-            audioMixer.SetFloat("BGM", 30.0f);
+            mAudioMixer.SetFloat("SE", 30.0f);
+            mAudioMixer.SetFloat("BGM", 30.0f);
 
-            vol1.SetActive(true);
-            vol2.SetActive(true);
-            vol3.SetActive(true);
-            vol4.SetActive(true);
+            gVol_1.SetActive(true);
+            gVol_2.SetActive(true);
+            gVol_3.SetActive(true);
+            gVol_4.SetActive(true);
 
-            NowVol++;
+            iNowVol++;
         }
     }
     // 音量DOWN
     public void OnVolDown()
     {
-        if (NowVol == 2)
+        if (iNowVol == 2)
         {
             Debug.Log("音量１に");
-            audioMixer.SetFloat("SE", -15.0f);
-            audioMixer.SetFloat("BGM", -15.0f);
-            vol1.SetActive(true);
-            vol2.SetActive(false);
-            vol3.SetActive(false);
+            mAudioMixer.SetFloat("SE", -15.0f);
+            mAudioMixer.SetFloat("BGM", -15.0f);
+            gVol_1.SetActive(true);
+            gVol_2.SetActive(false);
+            gVol_3.SetActive(false);
+            gVol_4.SetActive(false);
 
-            NowVol--;
+            iNowVol--;
         }
-        else if (NowVol == 3)
+        else if (iNowVol == 3)
         {
             Debug.Log("音量２に");
-            audioMixer.SetFloat("SE", 0.0f);
-            audioMixer.SetFloat("BGM", 0.0f);
+            mAudioMixer.SetFloat("SE", 0.0f);
+            mAudioMixer.SetFloat("BGM", 0.0f);
 
-            vol1.SetActive(true);
-            vol2.SetActive(true);
-            vol3.SetActive(false);
+            gVol_1.SetActive(true);
+            gVol_2.SetActive(true);
+            gVol_3.SetActive(false);
+            gVol_4.SetActive(false);
 
-            NowVol--;
+            iNowVol--;
         }
-        else if (NowVol == 4)
+        else if (iNowVol == 4)
         {
             Debug.Log("音量３に");
-            audioMixer.SetFloat("SE", 15.0f);
-            audioMixer.SetFloat("BGM", 15.0f);
+            mAudioMixer.SetFloat("SE", 15.0f);
+            mAudioMixer.SetFloat("BGM", 15.0f);
 
-            vol1.SetActive(true);
-            vol2.SetActive(true);
-            vol3.SetActive(true);
+            gVol_1.SetActive(true);
+            gVol_2.SetActive(true);
+            gVol_3.SetActive(true);
+            gVol_4.SetActive(false);
 
-            NowVol--;
+            iNowVol--;
         }
     }
 
     //設定ボタンのパネルの表示オフ
     public void OnDisappearSettingButton()
     {
-        setting_buttonObj.SetActive(false);
+        gSettingObj.SetActive(false);
     }
     //設定ボタンのパネルの表示オン
     public void OnClickSettingButton()
     {
-        setting_buttonObj.SetActive(true);
+        gSettingObj.SetActive(true);
     }
 }
