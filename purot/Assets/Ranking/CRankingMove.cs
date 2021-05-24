bdb54dfabbeeb54780fa84a8b0d844cfc81cfb55
yukkedone <sukkechi@gmail.com>
2021-05-24 17:19:14 +0900

    横2列から横1列に変えた。名前の表記の部分は未実装。

--- purot/Assets/Ranking/CRankingMove.cs
+++ purot/Assets/Ranking/CRankingMove.cs
@@ -11,23 +11,47 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 
 public class CRankingMove : MonoBehaviour
 {
+    private GameObject[] RankObject = new GameObject[10];
+    private GameObject[] NameObject = new GameObject[10];
+    private GameObject[] ScoreObject = new GameObject[10];
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        SetGameObject();
+
+        SetPosition();
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKeyDown(KeyCode.Escape))
+    }
+
+    void SetGameObject()
+    {
+        for(int i = 0; i < 10; i++)
         {
-            SceneManager.LoadScene("TitleScene");
+            RankObject[i] = GameObject.Find("top" + (i + 1));
+            RankObject[i].GetComponent<RectTransform>().localScale = new Vector2(0.2f, 0.2f);
+            ScoreObject[i] = GameObject.Find("score" + (i + 1));
+            ScoreObject[i].GetComponent<RectTransform>().localScale = new Vector2(0.2f, 0.2f);
+            // –¼‘OŒã‚Å’Ç‰Á
+        }
+    }
+
+    void SetPosition()
+    {
+        for(int i = 0; i < 10; i++)
+        {
+            RankObject[i].GetComponent<RectTransform>().position = new Vector3(-65.0f, 42.0f + i * -11.0f, 100.0f);
+            ScoreObject[i].GetComponent<RectTransform>().position = new Vector3(-10.0f, 41.0f + i * -11.0f, 100.0f);
         }
     }
 }
