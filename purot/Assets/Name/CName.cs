f0dbb5017a343a76c1df62d370a56b8c384761bd
yukkedone <sukkechi@gmail.com>
2021-06-28 17:58:57 +0900

    ランキング足りなかったやつ

--- purot/Assets/Name/CName.cs
+++ purot/Assets/Name/CName.cs
@@ -1,26 +1,27 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class CName : MonoBehaviour{
 
     [SerializeField] int iNamenumber;
     [SerializeField] public Sprite[] word = new Sprite[256];
-    SpriteRenderer sr;
+    Image Img;
     public GameObject gObject;
     public CNameManager manager;
     
     void Start() {
         gObject = GameObject.Find("PFB_Words");
         manager = gObject.GetComponent<CNameManager>();
-      
-        sr = gameObject.GetComponent<SpriteRenderer>();
-        sr.sprite = word[manager.iSavename[iNamenumber]]; 
+
+        Img = gameObject.GetComponent<Image>();
+        Img.sprite = word[manager.iSavename[iNamenumber]]; 
     }
 
     void Update(){
 
-        sr.sprite = word[manager.iSavename[iNamenumber]];
+        Img.sprite = word[manager.iSavename[iNamenumber]];
 
     }
 
